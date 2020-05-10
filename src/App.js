82d54244260187a544128d29897f4dde8231d51e
TobyChen0106 <awesdjzo@gmail.com>
2020-05-10 17:55:19 +0800

    home page done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import DashboardRouter from "./components/DashboardRouter/DashboardRouter";
+import { Cookies } from 'react-cookie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cookies = new Cookies();
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userEmail: cookies.get('cardbo-user-email'),
+      userPassword: cookies.get('cardbo-user-password'),
+    }
+    console.log(this.state.userEmail);
+    console.log(this.state.userPassword);
+  }
+  onSavePassWord = () => {
+
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <DashboardRouter onSavePassWord={this.onSavePassWord}/>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
---
+++ src/components/DashboardRouter/listItems.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+import HomeIcon from '@material-ui/icons/Home';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleIcon from '@material-ui/icons/People';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import SettingsIcon from '@material-ui/icons/Settings';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+import { Link, Router } from "react-router-dom";
+
+// import Rewards from "../../containers/Rewards";
+
+
+export const mainListItems = (
+  <div>
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/">
+      <ListItem button>
+        <ListItemIcon>
+          <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary="Home" />
+      </ListItem>
+    </Link>
+
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/Dashboard">
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </Link>
+
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/Rewards">
+      <ListItem button>
+        <ListItemIcon>
+          <ShoppingBasketIcon />
+        </ListItemIcon>
+        <ListItemText primary="Rewards" />
+      </ListItem>
+    </Link>
+
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/CreditCards">
+      <ListItem button>
+        <ListItemIcon>
+          <CreditCardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Credit Cards" />
+      </ListItem>
+    </Link>
+
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/Banks">
+      <ListItem button>
+        <ListItemIcon>
+          <AccountBalanceIcon />
+        </ListItemIcon>
+        <ListItemText primary="Banks" />
+      </ListItem>
+    </Link>
+
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/Users">
+      <ListItem button>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Users" />
+      </ListItem>
+    </Link>
+
+    <Link style={{ textDecoration: 'none', color: "#000000", }} to="/Setting">
+      <ListItem button>
+        <ListItemIcon>
+          <SettingsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Setting" />
+      </ListItem>
+    </Link>
+
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListSubheader inset>Reports</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Report 1" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Report 2" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Report 3" />
+    </ListItem>
+  </div>
+);
