ce28044060ee29b8711022b8c9718ffc2c3fe2a2
TobyChen0106 <awesdjzo@gmail.com>
2020-05-29 04:36:26 +0800

    fix module

--- src/route/api.js
+++ src/route/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/User');
 const Card = require('../models/Card');
-const Offer = require('../models/offer');
+const Offer = require('../models/Offer');
 const Store = require('../models/Store');
 
 // router.post('/users', (req, res) => {
